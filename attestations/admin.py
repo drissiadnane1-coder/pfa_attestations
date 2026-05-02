e107b2e363ddcbd76f17345142107d5d570af733
Adnane DRISSI <drissiadnane1@gmail.com>
2026-05-03 00:44:05 +0100

    Add attestation models and admin configuration

--- attestations/admin.py
+++ attestations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    TypeAttestation,
+    ModeleAttestation,
+    DemandeAttestation,
+    Attestation,
+    FichierPDF,
+    QRCodeAttestation,
+    ArchiveAttestation,
+    VerificationAttestation,
+    JournalAction,
+)
 
-# Register your models here.
+
+admin.site.register(TypeAttestation)
+admin.site.register(ModeleAttestation)
+admin.site.register(DemandeAttestation)
+admin.site.register(Attestation)
+admin.site.register(FichierPDF)
+admin.site.register(QRCodeAttestation)
+admin.site.register(ArchiveAttestation)
+admin.site.register(VerificationAttestation)
+admin.site.register(JournalAction)
